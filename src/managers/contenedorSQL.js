75de8ce68d551d7f375b83c96043e4fd1b0e04bb
Adra Omar <adra.omar2@gmail.com>
2022-12-04 05:55:39 -0300

    Añadido funciones restantes a ContenedorSQL

--- src/managers/contenedorSQL.js
+++ src/managers/contenedorSQL.js
@@ -6,7 +6,29 @@ class ContenedorSQL{
         this.table = tableName;
     }
 
-    async getAll() {
+    save = async(object) => {
+        try {
+            const response = await this.database.from(this.table).insert(object);
+            const result = JSON.parse(JSON.stringify(response));
+            
+            return `Objeto agregado correctamente - ID: ${result}`;
+        } catch (error) {
+            return `Error: ${error}`;
+        }
+    }
+
+    getById = async(id) => {
+        try {
+            const response = await this.database.from("productos").select("*").where("id", "=", id);
+            const result = JSON.parse(JSON.stringify(response));
+
+            return result;
+        } catch (error) {
+            return `Error: ${error}`;
+        }
+    }
+
+    getAll = async() => {
         try {
             const response = await this.database.from(this.table).select("*");
             const result = JSON.parse(JSON.stringify(response));
@@ -17,11 +39,33 @@ class ContenedorSQL{
         }
     }
 
-    async save(product) {
+    deleteById = async(id) => {
         try {
-            const [id] = await this.database.from(this.table).insert(product);
-            
-            return `Producto agregado correctamente - ID: ${id}`;
+            const response = await this.database.from(this.table).where("id", "=", id).del();
+            const result = JSON.parse(JSON.stringify(response));
+
+            return `Objeto eliminado correctamente - ID: ${result}`;
+        } catch (error) {
+            return `Error: ${error}`;
+        }
+    }
+
+    deleteAll = async() => {
+        try {
+            const response = await this.database.from(this.table).del();
+
+            return `Todos los objetos han sido eliminado correctamente`;
+        } catch (error) {
+            return `Error: ${error}`;
+        }
+    }
+
+    updateById = async(id, body) => {
+        try {
+            const response = this.database.from("cars").select("*").where("id", "=", id).update(body)
+            const result = JSON.parse(JSON.stringify(response));
+
+            return `Objeto actualizado correctamente - ID: ${result}`;
         } catch (error) {
             return `Error: ${error}`;
         }
